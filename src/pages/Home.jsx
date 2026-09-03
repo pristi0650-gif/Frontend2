7452ffa23156a141747aec7f327d7183b71fc744
PristiShrestha <pristishrestha0650@gmail.com>
2026-09-03 12:53:21 +0545

    files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Hero from './hero'
+import Hero from './Hero'
 import About from './About'
 import Project from './Project'
 import Contacts from './Contacts'
